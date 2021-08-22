1c11fffb2663d3723426558c1cd791abce331052
ccat3z <fzhang.chn@foxmail.com>
2021-08-22 09:43:29 +0800

    Update default markup for button

--- argos@pew.worldwidemann.com/button.js
+++ argos@pew.worldwidemann.com/button.js
@@ -35,7 +35,7 @@ class ArgosButton extends PanelMenu.Button {
     this._updateInterval = settings.updateInterval;
 
     this._lineView = new ArgosLineView();
-    this._lineView.setMarkup("<small><i>" + GLib.markup_escape_text(file.get_basename(), -1) + " ...</i></small>");
+    this._lineView.setMarkup("<small><i>...</i></small>");
     Utilities.getActor(this).add_actor(this._lineView);
 
     this._isDestroyed = false;
